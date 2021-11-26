6c2beb5edf08695a1136b9c851e358a239119d52
lwa19 <leahwang19@gmail.com>
2021-11-25 19:46:06 -0500

    Separated Dijkstra's Algorithm

---
+++ R/between.R
@@ -0,0 +1,11 @@
+# Betweenness Centrality Calculation
+between = function(A.mat, weight = F){
+  # make graph binary if we don't care about weights:
+  if (!weight) {
+    A.mat = ifelse(A.mat != 0, 1, 0)
+  }
+  diag(A.mat) = 0      # remove self-loops
+  n = dim(A.mat)[2]
+
+
+}
--- R/closeness.R
+++ R/closeness.R
@@ -1,4 +1,4 @@
-# closeness centrality computation (NEED MORE WORK; NEED TO CLEAN UP PRINTS)
+# closeness centrality computation (NEED TO CLEAN UP PRINTS)
 # "geodesic" distances is calculated by the Dijkstra's Algorithm
 closeness = function(A.mat, weight = F){
   # make graph binary if we don't care about weights:
@@ -28,43 +28,4 @@ closeness = function(A.mat, weight = F){
   di = sapply(1:n, FUN = dijkstra, A.mat = A.mat)
   c.closeness.std = 1/di
   return(c.closeness.std)
-}
-
-
-dijkstra = function(A.mat, src){
-  n = dim(A.mat)[2]
-  vertex = 1:n
-  dist = rep(Inf, n)
-  dist[src] = 0
-  prev = rep(NA, n)
-
-  while (any(!is.na(vertex))){
-    # print('vertex')
-    # print(vertex)
-    u = which(dist == min(dist[vertex], na.rm = T))
-    vertex[u] = NA
-
-    # find the closest neighbor to src
-    v = which(A.mat[u,] != 0)
-    if (length(v) == 0){
-      break
-    }
-    for (ind in v){
-      # print('u')
-      # print(u)
-      # print('ind')
-      # print(ind)
-      # print('dist')
-      # print(dist)
-      alt = dist[u] + A.mat[u,ind]
-      # print('alt')
-      # print(alt)
-      if (alt < dist[ind]){
-        dist[ind] = alt
-      }
-    }
-  }
-
-  # return the mean of the distances
-  return(mean(dist))
 }
