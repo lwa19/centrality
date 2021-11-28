f3dc490b7b49be11f90cbd692541b11505f03910
lwa19 <leahwang19@gmail.com>
2021-11-28 02:06:40 -0500

    backing up namespace file. wiping for roxygen

--- R/degree.R
+++ R/degree.R
@@ -1,5 +1,20 @@
-# degree centrality computation
-degree = function(A.mat, type = 'undirected', dir = F){
+#' Degree Centrality Computation
+#'
+#' `degree` calculates the degree centrality of the network
+#'
+#' @param A.mat An n x n adjacency matrix.
+#' @param type A character string indicating the type of degree
+#' centrality: "undirected", "indegree", or "outdegree".
+#' By default, it is "undirected".
+#'
+#' @return Returns the value of `c.degree.std`
+#' @examples
+#'
+#' myfunction(1) # returns 1
+#'
+#' @export
+
+degree = function(A.mat, type = 'undirected'){
   # convert graphs with weighted edges into binary
   A.mat = ifelse(A.mat != 0, 1, 0)
   diag(A.mat) = 0      # remove self-loops
