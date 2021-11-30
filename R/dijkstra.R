c6bca95fd39223698850c540648fd6f80c41fde0
lwa19 <leahwang19@gmail.com>
2021-11-29 22:34:43 -0500

    The Dijkstra algorithm is too perfect. Anything I do to it feels like blasphemy.

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -4,8 +4,6 @@
 #'
 #' @param A.mat An n x n adjacency matrix.
 #' @param src A length 1 vector indicating the start node
-#' @param snk A length 1 vector indicating the end node
-#'
 #' @return Returns a list of `dist` and `paths`
 #' @examples
 #'
@@ -13,10 +11,8 @@
 #'
 #' @export
 
-# Dijkstra's Algorithm (NEED WORK)
-# (used for geodesic distance calculation in closeness.R and between.R)
-# Maybe it's better to do src-snk instead?
-dijkstra = function(A.mat, src, snk){
+# Deprecated all nodes computation
+dijkstra = function(A.mat, src){
   n = dim(A.mat)[2]
   vertex = 1:n
   dist = rep(Inf, n)
@@ -61,6 +57,83 @@ dijkstra = function(A.mat, src, snk){
         dist[ind] = alt
         path[[ind]] = c(path[[ind]], u)
       }
+    }
+  }
+
+  # return the mean of the distances
+  # return(mean(dist))
+  # return a list of distances and paths:
+  return(list(dist = dist, path = path))
+}
+
+
+
+
+
+
+# Dijkstra's Algorithm (THE CLEARLY NOT WORKING VERSION)
+# (used for geodesic distance calculation in closeness.R and between.R)
+# Maybe it's better to do src-snk instead?
+dijkstra_revise = function(A.mat, src){
+  n = dim(A.mat)[2]
+  vertex = 1:n                                    # vertices to visit
+  dist = rep(Inf, n)
+  dist[src] = 0                                   # distances from src
+  path = vector(mode = "list", length = n)        # geodesics for each src-snk pair
+
+  while (any(!is.na(vertex))){
+    #print('vertex')
+    #print(vertex)
+    min.pts = which(dist == min(dist[vertex], na.rm = T))
+    u = intersect(vertex, min.pts)               # skip passed points; could have multiple u
+    ties = length(u)
+
+    # find the closest neighbor to src
+    v = which(A.mat[u,] != 0, arr.ind = T)
+    if (length(v) == 0){
+      break
+    } else if (length(u) > 1){
+      ## move
+      geodesics = vector(mode = "list", length = num.geodesics)
+      # dist.geodesics = vector(mode = "list", length = num.geodesics)
+      ## end move
+
+      # tie breaker for similar min-distance
+      rows = unique(v[,1])
+      min.geodesics = Inf
+      for (r in 1:ties){
+        # dist.geodesics[[r]] = dist             # update with the current distance array
+        r.v = v[c(v[,1] == 2),2]
+        for (ind in r.v){
+          alt = dist[row[r]] + A.mat[row[r],ind]
+          if (alt < dist[ind] & alt < min.geodesics){
+            min.geodesics = alt
+            geodesics[[r]][[ind]] = c(path[[ind]], row[r])
+          }
+        }
+      }
+      dist[ind] = min(dist.geodesics)
+    } else {
+      for (ind in v){
+        #print('u')
+        #print(u)
+        #print('ind')
+        #print(ind)
+        #print('dist')
+        #print(dist)
+        alt = dist[u] + A.mat[u,ind]
+        #print('alt')
+        #print(alt)
+        if (alt < dist[ind]){
+          dist[ind] = alt
+          path[[ind]] = c(path[[ind]], u)
+        }
+    }
+    }
+
+    # remove u from the list of unvisited vertices
+    if (length(u) == 1){
+      vertex[u] = NA
     }
   }
 
--- R/sim_adjacency.R
+++ R/sim_adjacency.R
@@ -1,3 +1,22 @@
+# All the simulation functions you will ever need
+
+#' Adjacency matrix simulation
+#'
+#' `sim_adjacency` creates a randomly generated adjacency matrix
+#'
+#' @param n Positive integer, number of nodes in the network
+#' @param mode A character string, indicating directedness of the matrix.
+#' Default is "undirected", where edges do not have direction (therefore adjacency
+#' matrix symmetric)
+#' @param weight A length-2 vector of non-negative integers, specifying the range
+#' edge weights. Default is `NULL`, indicating unweighted edges.
+#' @return Returns an n x n adjacency matrix `A.mat`
+#' @examples
+#'
+#' myfunction(1) # returns 1
+#'
+#' @export
+
 # Simulate an adjacency matrix with no self loops
 sim_adjacency = function(n, mode = "undirected", weight = NULL){
   A.entries = n * n
@@ -23,3 +42,26 @@ sim_adjacency = function(n, mode = "undirected", weight = NULL){
   return(A.mat)
 }
 
+
+#' Adjacency matrix simulation -- varying connectedness
+#'
+#' `sim_deg_conn` creates a randomly generated adjacency matrix with different degree of connectedness
+#'
+#' @param n Positive integer, number of nodes in the network
+#' @param p Positive double, probability of a node having a connection with another.
+#' Default is `0.5`.
+#' @return Returns an n x n adjacency matrix `A.mat`
+#' @examples
+#'
+#' myfunction(1) # returns 1
+#'
+#' @export
+
+# simulate undirected- unweighted-graph with degree of connectedness
+sim_deg_conn = function(n, p = 0.5){
+  A.entries = n * n
+  A.nums = rbinom(A.entries, 1, p)
+  diag(A.mat) = 0
+  A.mat[lower.tri(A.mat)] = t(A.mat)[lower.tri(A.mat)]
+  return(A.mat)
+}
