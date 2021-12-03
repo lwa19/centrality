145bd6624464acbf5fcdb8b3ccd86ba3debb3bc0
lwa19 <leahwang19@gmail.com>
2021-12-02 19:04:26 -0500

    fixed bus after reverting

--- R/sim_adjacency.R
+++ R/sim_adjacency.R
@@ -62,6 +62,7 @@ sim_adjacency = function(n, mode = "undirected", weight = NULL){
 sim_deg_conn = function(n, p = 0.5){
   A.entries = n * n
   A.nums = rbinom(A.entries, 1, p)
+  A.mat = matrix(A.nums, nrow = n)
   diag(A.mat) = 0
   A.mat[lower.tri(A.mat)] = t(A.mat)[lower.tri(A.mat)]
   return(A.mat)
