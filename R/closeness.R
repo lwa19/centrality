1c4b2de897d58ede0e60512ecb08b0e1fe9289d3
lwa19 <leahwang19@gmail.com>
2021-12-04 03:22:21 -0500

    if R crashes one more time I will riot

--- R/closeness.R
+++ R/closeness.R
@@ -4,25 +4,16 @@
 #' required to access every other node from a given node.
 #'
 #' @param A.mat An n x n adjacency matrix.
-#' @param weight A boolean indicating if edges are weighted;
-#' default is FALSE.
 #' @return Returns a length-n vector of `c.closeness.std`
 #' of standardized closeness centrality scores
 #' @examples
-#' A.mat.unw = sim_adjacency(10) # edges unweighted
-#' closeness(A.mat.unw)
-#'
 #' A.mat.w = sim_adjacency(10, weight = c(1, 10))
-#' closeness(A.mat.w, weight = TRUE)
+#' closeness(A.mat.w)
 #'
 #' @export
 
 # "geodesic" distances is calculated by the Dijkstra's Algorithm
-closeness = function(A.mat, weight = F){
-  # make graph binary if we don't care about weights:
-  if (!weight) {
-    A.mat = ifelse(A.mat != 0, 1, 0)
-  }
+closeness = function(A.mat){
   diag(A.mat) = 0      # remove self-loops
   n = dim(A.mat)[2]
 
